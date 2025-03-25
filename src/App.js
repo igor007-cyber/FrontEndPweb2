817a937a2f2a5992dafc4060bc9eb920947323bd
igor007-cyber <igor.ramalho007@gmail.com>
2025-03-25 15:31:03 -0300

    ajeitando o taiwilnmd

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './screens/Home/home';
 import Login from './screens/Login/login';
 import Cadastro from './screens/Cadastro/cadastro';
+import Produto from './screens/Produtos/produto';
+import './index.css';
+import Dashboard from './screens/Dashboard';
 
 function App() {
     return (
@@ -11,6 +14,8 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/produto" element={<Produto />} />\
+                <Route path="/Dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
     );
