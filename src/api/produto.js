8d49a78844c3fcaa11f571e7eb9970abe4b8ce6f
thalia-araujoo <liahsouza1998@mail.com>
2025-03-28 16:14:20 -0300

    atualizei o produto.js dentro da api

--- src/api/produto.js
+++ src/api/produto.js
@@ -2,5 +2,12 @@ import api from "./api"
 //import {jwtDecode} from "jwt-decode";
 
 export const cadastros = async (nome, telefone, cpf, rua, bairro, cidade, email, senha) => {
-   
+    try{
+        const response = await api.post('/cliente/registro', {nome, telefone, cpf, rua, bairro, cidade, email, senha});
+        console.log(response.data);
+        return response.data;
+    }catch(error){
+        console.error("Erro no cadastro:", error);
+        throw new Error('Falha no cadastro');
+    }
 }
