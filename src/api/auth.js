f5060119d82b0d3ac3ef1b522a16144ac8296b80
thalia-araujoo <liahsouza1998@mail.com>
2025-03-28 16:09:13 -0300

    atualizei a auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,10 +3,11 @@ import {jwtDecode} from "jwt-decode";
 
 export const login = async (email, senha) => {
     try{
-        const response = await api.post('/login', {email, senha});
+        const response = await api.post('/api/login', {email, senha});
         const { token } = response.data;
         const decode = jwtDecode(token);
         localStorage.setItem('token', token);
+        localStorage.setItem('user', JSON.stringify(response.data.user));
         localStorage.setItem('userType', decode.tipo);
     }catch(error){
         throw new Error('Falha na autenticação')
